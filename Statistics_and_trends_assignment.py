67c0f859accee2ed3fed05fecc51022e3493d9a9
divyamuraleedharan12 <125889448+divyamuraleedharan12@users.noreply.github.com>
2023-04-04 23:07:30 +0100

    Add files via upload
    Added bar plot code

--- Statistics_and_trends_assignment.py
+++ Statistics_and_trends_assignment.py
@@ -6,6 +6,7 @@ Created on Sun Apr 02 20:18:17 2023
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def read_data(filename):
@@ -43,3 +44,49 @@ def filter_data(dataframe, column, value, countries, years):
     transposed_data = transposed_data.transpose()
     # Returns normal dataframe and transposed dataframe
     return filtered_data, transposed_data
+
+
+def bar_plot(dataset, title, xlabel, ylabel):
+    """ 
+    The function below is used to generate a bar plot.
+    """
+    # Creates a bar plot from the dataset DataFrame
+    dataset.plot.bar(x='Country Name', rot=0, figsize=(50, 25), fontsize=50)
+    # Sets the location of the y-axis ticks
+    plt.yticks([0, 20, 40, 60, 80, 100])
+    plt.legend(fontsize=50)
+    # Sets title and font size for plot
+    plt.title(title.upper(), fontsize=60, fontweight='bold')
+    # Sets x-label and font size for plot axes
+    plt.xlabel(xlabel, fontsize=60)
+    # Sets y-label and font size for plot axes
+    plt.ylabel(ylabel, fontsize=60)
+    # Saves bar plot figure as png
+    plt.savefig(title + '.png')
+    # Function to show the plot
+    plt.show()
+    return
+
+
+# Creating list of countries and years for plotting bar plot
+country1 = ['Ethiopia', 'Peru', 'India', 'Nigeria']
+year1 = ['2000', '2005', '2010', '2015', '2020']
+# Reads data from csv file
+world_data = read_data("climate_change.csv")
+world_data1, transdata1 = filter_data(
+    world_data, 'Indicator Name', 'Urban population (% of total population)', country1, year1)
+# Prints filtered data and transposed data
+print(world_data1)
+print(transdata1)
+# Calling bar plot function with indicator as Urban Population
+bar_plot(world_data1, 'Urban population (% of total population)',
+         'Countries', 'Percentage of Urban Population')
+
+world_data2, transdata2 = filter_data(
+    world_data, 'Indicator Name', 'Access to electricity (% of population)', country1, year1)
+
+print(world_data2)
+print(transdata2)
+# Calling another bar plot function with indicator as Access to electricity
+bar_plot(world_data2, 'Access to Electricity (% of population)',
+         'Countries', 'Percentage of Electricity access')
